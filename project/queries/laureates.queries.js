51de647572fdbadb7289db9d209285aa9b5662b3
Maxime Crayssac <98148520+mcrayssac@users.noreply.github.com>
2022-11-30 17:17:42 +0100

    Delete project directory

--- project/queries/laureates.queries.js
+++
@@ -1,6 +0,0 @@
-const list_laureates = "SELECT * FROM LAUREATE;";
-const list_laureate = "SELECT firstname,surname, PrizeWin FROM LAUREATS  WHERE id = $1 AND PrizeWin = (SELECT year FROM PRIZE INNER JOIN LAUREATS ON PRIZE.id_laureat = LAUREATS.id INNER JOIN INNER JOIN LAUREATS ON RECOIT.idLaureat = LAUREATS.id_laureat);";
-
-module.exports = {
-    list_laureates
-}
